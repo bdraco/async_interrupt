92b8e6906d7600d415f32f8d9306f6fd5388e09c
J. Nick Koston <nick@koston.org>
2023-07-20 11:10:39 -0500

    feat: first version (#1)

---
+++ tests/test_init.py
@@ -0,0 +1,83 @@
+import asyncio
+
+import pytest
+
+from async_interrupt import _Interrupt, interrupt
+
+
+@pytest.mark.asyncio
+async def test_interrupt_immediate():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+
+    with pytest.raises(ValueError, match="message"):
+        async with interrupt(future, ValueError, "message"):
+            future.set_result(None)
+            await asyncio.sleep(0)
+
+
+@pytest.mark.asyncio
+async def test_interrupt_immediate_no_message():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+
+    with pytest.raises(ValueError):
+        async with interrupt(future, ValueError, None):
+            future.set_result(None)
+            await asyncio.sleep(0)
+
+
+@pytest.mark.asyncio
+async def test_interrupt_soon():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+
+    with pytest.raises(ValueError, match="message"):
+        async with interrupt(future, ValueError, "message"):
+            loop.call_soon(future.set_result, None)
+            await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_interrupt_later():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+
+    with pytest.raises(ValueError, match="message"):
+        async with interrupt(future, ValueError, "message"):
+            loop.call_later(0.001, future.set_result, None)
+            await asyncio.sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_interrupt_does_not_happen():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+
+    async with interrupt(future, ValueError, "message"):
+        await asyncio.sleep(0.001)
+
+
+@pytest.mark.asyncio
+async def test_interrupt_does_not_happen_after_context_exit():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+    loop.call_soon(future.set_result, None)
+
+    async with interrupt(future, ValueError, "message"):
+        await asyncio.sleep(0)
+
+
+@pytest.mark.asyncio
+async def test_reuse_not_allowed():
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
+    loop.call_soon(future.set_result, None)
+    manual_interrupt = _Interrupt(loop, future, ValueError, "message")
+
+    async with manual_interrupt:
+        await asyncio.sleep(0)
+
+    with pytest.raises(RuntimeError):
+        async with manual_interrupt:
+            await asyncio.sleep(0)
